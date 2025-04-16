640cf4e228b2bbc08e79a12f516f2c2e7c32c3ac
Satyam Dwivedi <122256218+satyam7579@users.noreply.github.com>
2025-04-16 16:38:29 +0530

    feat: sent user id after successful login

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -29,8 +29,9 @@ const login = async (req, res) => {
     if (!isPasswordValid) {
       return res.status(401).json({ error: "Invalid password" });
     }
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
-    res.status(200).json({ message: "Login successful", token });
+    const id = user._id;
+    const token = jwt.sign({ id: id }, process.env.JWT_SECRET);
+    res.status(200).json({ id, message: "Login successful", token });
   } catch (err) {
     res.status(400).json({ error: err.message });
   }
@@ -46,7 +47,6 @@ const getUserInfo = async (req, res) => {
     if (!user) {
       return res.status(404).json({ error: "User not found" });
     }
-
     res.status(200).json(user);
   } catch (err) {
     res.status(500).json({ error: err.message });
